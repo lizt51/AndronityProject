c4e232b9ee947ada8ab847457efa31d94058b83f
SulistyoW <sulistyo.wahyu27@gmail.com>
2018-04-29 18:14:16 +0700

    desain login

--- app/src/main/java/com/example/hyde/andronityproject/MainActivity.kt
+++ app/src/main/java/com/example/hyde/andronityproject/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.hyde.andronityproject
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.hyde.andronityproject.helper.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContent()
+
+    }
+
+    private fun setContent() {
+        val vpa = ViewPagerAdapter(supportFragmentManager)
+        vpa.addFragment(SigninFragmnet(), "SIGN IN")
+        vpa.addFragment(SignupFragment(), "SIGN UP")
+        pager.adapter = vpa
+        tab.setupWithViewPager(pager)
     }
 }
